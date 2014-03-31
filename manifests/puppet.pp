fe252694c0a1e18fb5b223646961c9d7d0ee9a31
Högni Gylfason <klumhru@gmail.com>
2014-03-31 11:49:27 +0000

    Move variables to vagrant file

--- manifests/puppet.pp
+++ manifests/puppet.pp
@@ -1,11 +1,3 @@
-$www_root = "/var/www"
-
-$project_name = 'unimods'
-
-$project_home = "$www_root/$project_name"
-
-$venv_home = "$project_home/venv.$project_name"
-
 include nginx
 
 group { "puppet":
@@ -22,6 +14,7 @@ file { $www_root:
     mode => '2775';
 }
 file { $project_home:
+    owner => 'vagrant', group => 'vagrant',
     ensure => directory,
     mode => '2775';
 }
@@ -44,6 +37,14 @@ class { "postgresql::globals":
 class { 'postgresql::server':
     ensure => present,
 } ->
+postgresql::server::role { $db_user:
+    password_hash => postgresql_password($db_user, $db_pass),
+    login => true,
+    createdb => true,
+} ->
+postgresql::server::database { $db_name:
+    owner => $db_user,
+}
 package { 'libpq-dev':
     ensure => present,
 } ->
@@ -68,14 +69,24 @@ upstart::job { "gunicorn_$project_name":
     ensure         => 'present',
     user           => 'www-data',
     group          => 'www-data',
+    environment    => {
+        'DB_NAME' => $db_name,
+        'DB_USER' => $db_user,
+        'DB_PASS' => $db_pass,
+    },
     chdir          => "$project_home/$project_name",
-    exec           => "$venv_home/bin/python manage.py run_gunicorn -w 4 -k gevent --max-requests 1",
+    exec           => "$venv_home/bin/python manage.py run_gunicorn -w 4 -k gevent -b $gunicorn_socket --max-requests 1",
 } ->
-nginx::resource::upstream { 'gunicorn_unimods':
-    members => [
-        'localhost:8000'
-    ]
+file { "nginx_vhost_$project_name":
+    owner => vagrant, group => vagrant,
+    path => "$project_home/conf/nginx_vhost.conf",
+    content => template("$project_home/manifests/nginx_vhost.conf.erb"),
 } ->
-nginx::resource::vhost { 'mods.amyingoh.org':
-    proxy => 'http://gunicorn_unimods'
+file { "nginx_vhost_$project_name-link":
+    owner => root, group => root,
+    ensure => link,
+    path => "/etc/nginx/sites-enabled/$project_name.vhost.conf",
+    target => "$project_home/conf/nginx_vhost.conf",
 }
+
+File["nginx_vhost_$project_name"] ~> Service['nginx']
