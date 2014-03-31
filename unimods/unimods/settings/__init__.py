4d821e41d116b4904458301e12cdda1b53e6157e
Högni Gylfason <klumhru@gmail.com>
2014-03-31 11:50:02 +0000

    Import config from external files

--- unimods/unimods/settings/__init__.py
+++ unimods/unimods/settings/__init__.py
@@ -8,10 +8,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.6/ref/settings/
 """
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+from .paths import *
 
+from .databases import *
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
@@ -56,17 +55,6 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'unimods.urls'
 
 WSGI_APPLICATION = 'unimods.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
